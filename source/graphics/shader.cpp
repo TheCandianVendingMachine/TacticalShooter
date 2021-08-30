2a982f580a629063ffaf34c673c31ed8d113775f
Brandon Danyluk <brandondanyluk366@gmail.com>
2021-08-30 17:33:54 -0600

    helper functions for setting uniforms

--- source/graphics/shader.cpp
+++ source/graphics/shader.cpp
@@ -5,6 +5,7 @@
 #include <glad/glad.h>
 #include <iostream>
 #include <spdlog/spdlog.h>
+#include <glm/gtc/type_ptr.hpp>
 
 shader::shader(const char *vertexPath, const char *fragmentPath)
 	{
@@ -61,5 +62,35 @@ shader::~shader()
 void shader::use()
 	{
 		glUseProgram(id);
+	}
+
+void shader::setInt(const char *uniform, int value) const
+	{
+		glUniform1i(glGetUniformLocation(m_id, uniform), value);
+	}
+
+void shader::setFloat(const char *uniform, float value) const
+	{
+		glUniform1f(glGetUniformLocation(m_id, uniform), value);
+	}
+
+void shader::setBool(const char *uniform, bool value) const
+	{
+		glUniform1i(glGetUniformLocation(m_id, uniform), value);
+	}
+
+void shader::setVec2(const char *uniform, const glm::vec2 &value) const
+	{
+		glUniform2fv(glGetUniformLocation(m_id, uniform), 1, glm::value_ptr(value));
+	}
+
+void shader::setVec3(const char *uniform, const glm::vec3 &value) const
+	{
+		glUniform3fv(glGetUniformLocation(m_id, uniform), 1, glm::value_ptr(value));
+	}
+
+void shader::setMat4(const char *uniform, const glm::mat4 &value) const
+	{
+		glUniformMatrix4fv(glGetUniformLocation(m_id, uniform), 1, GL_FALSE, glm::value_ptr(value));
 	}
 
--- source/graphics/shader.hpp
+++ source/graphics/shader.hpp
@@ -1,6 +1,9 @@
 // shader.hpp
 // allows the loading of a shader program
 #pragma once
+#include <glm/vec2.hpp>
+#include <glm/vec3.hpp>
+#include <glm/mat4x4.hpp>
 
 class shader
 	{
@@ -14,4 +17,11 @@ class shader
 			~shader();
 
 			void use();
+
+			void setInt(const char *uniform, int value) const;
+			void setFloat(const char *uniform, float value) const;
+			void setBool(const char *uniform, bool value) const;
+			void setVec2(const char *uniform, const glm::vec2 &value) const;
+			void setVec3(const char *uniform, const glm::vec3 &value) const;
+			void setMat4(const char *uniform, const glm::mat4 &value) const;
 	};
